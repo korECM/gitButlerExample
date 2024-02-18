35f8bc518ca624d29a6380871e83081de7db7e6e
Jongwon Youn <youncookiexpire@kakao.com>
2024-02-18 17:16:47 +0900

    feat: generate random name for Person when born instead of using a fixed name

--- src/Person.kt
+++ src/Person.kt
@@ -1,7 +1,7 @@
 class Person private constructor(val name: String) {
     companion object {
         fun born(): Person {
-            return Person("Mark")
+            return Person(generateRandomString(10))
         }
     }
 }
